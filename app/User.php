bf923ff2a1be210c6a4d79a8299e8c4835bd3935
Ivan Boldyrev <iboldurev@gmail.com>
2018-12-07 12:38:44 +0200

    Add team to user

--- app/User.php
+++ app/User.php
@@ -3,28 +3,28 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    /** {@inheritdoc} */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    /** {@inheritdoc} */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
 }
