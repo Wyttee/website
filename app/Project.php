692edef5cb67dfe2f294a8ba40d32e5540d32552
Ivan Boldyrev <iboldurev@gmail.com>
2018-12-07 12:20:29 +0200

    Added project resource

---
+++ app/Project.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Project extends Model
+{
+    use SoftDeletes;
+
+    /** {@inheritdoc} */
+    protected $table = 'projects';
+
+    /** {@inheritdoc} */
+    protected $fillable = [
+        'title', 'description', 'image_url',
+    ];
+}
